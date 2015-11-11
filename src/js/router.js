6bc357700f3c0e39882ac9c34069693e7a5e7591
evheniy <evheniy.bystrov@gmail.com>
2015-11-11 16:54:18 +0200

    navigation

--- src/js/router.js
+++ src/js/router.js
@@ -1,17 +1,29 @@
-var ViewManager = require('./viewManager'),
-    MainView = require('./mainView');
+var MainView = require('./mainView'),
+    TestView = require('./testView');
 module.exports = Backbone.Router.extend({
     initialize: function () {
-        Materialize.toast('I am a toast!', 4000);
+        Materialize.toast('Started!', 4000);
         Backbone.history.start({
             root: '/',
             pushState: true
         });
+        $(document).on("click","a",function(e){
+            e.preventDefault();
+            Backbone.history.navigate($(this).attr("href"),true);
+        });
     },
     routes: {
-        '' : 'main'
+        '' : 'main',
+        'test': 'test'
     },
-    main: function(){
-        ViewManager.showView(new MainView());
+    main: function() {
+        console.log('main');
+        Materialize.toast('Main!', 4000);
+        new MainView().render();
+    },
+    test: function () {
+        console.log('test');
+        Materialize.toast('Test!', 4000);
+        new TestView().render();
     }
 });
--- src/js/testView.js
+++ src/js/testView.js
@@ -1,8 +1,8 @@
-var tpl = _.template(require('html!./../templates/main.html'));
+var tpl = _.template(require('html!./../templates/test.html'));
 module.exports = Backbone.View.extend({
     el: $('#main'),
     render:function () {
-        this.$el.html(tpl({test: 'main'}));
+        this.$el.html(tpl({test: 'test'}));
         return this;
     }
 });
